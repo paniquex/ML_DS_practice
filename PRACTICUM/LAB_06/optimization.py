f441974d4aec8e8c4ec0c307d999c466251248d9
Nikita <paniquexx@gmail.com>
2019-11-17 00:32:00 +0300

    optimization module was done (need to understand step_beta and timing in GD and SGD)!

--- PRACTICUM/LAB_06/optimization.py
+++ PRACTICUM/LAB_06/optimization.py
@@ -1,10 +1,19 @@
+import oracles
+import numpy as np
+import scipy
+import scipy.special as spec
+import time
+
+
 class GDClassifier:
     """
     Реализация метода градиентного спуска для произвольного
     оракула, соответствующего спецификации оракулов из модуля oracles.py
     """
 
-    def __init__(self, loss_function, step_alpha=1, step_beta=0,
+    def __init__(self, loss_function='binary_logistic',
+                 step_alpha=1,
+                 step_beta=0,
                  tolerance=1e-5, max_iter=1000, **kwargs):
         """
         loss_function - строка, отвечающая за функцию потерь классификатора.
@@ -23,7 +32,21 @@ class GDClassifier:
 
         **kwargs - аргументы, необходимые для инициализации
         """
-        pass
+
+        if loss_function == 'binary_logistic':
+            if 'l2_coef' in kwargs.keys():
+                self.oracle = oracles.BinaryLogistic(l2_coef=kwargs['l2_coef'])
+            else:
+                self.oracle = oracles.BinaryLogistic()
+        self.step_alpha = step_alpha
+        self.step_beta = step_beta
+        self.tol = tolerance
+        self.max_iter = max_iter
+        self.w = None
+        self.X = None
+        self.y = None
+        self.trace = None
+        self.history = None
 
     def fit(self, X, y, w_0=None, trace=False):
         """
@@ -44,7 +67,46 @@ class GDClassifier:
         history['func']: list of floats, содержит значения функции на каждой итерации
         (0 для самой первой точки)
         """
-        pass
+
+        self.X = X
+        self.y = y
+        if w_0 is None:
+            self.w = np.random.uniform(0, 1, X.shape[1])  # need to make it with random
+        else:
+            self.w = w_0
+        self.trace = trace
+        if self.trace:
+            self.history = {'time': [], 'func': []}
+
+        # Algorithm
+        iteration = 0
+        start_time = time.time()
+        func_val_curr = self.get_objective(self.X, self.y)
+        grad_val = self.get_gradient(self.X, self.y)
+        self.w = (self.step_beta + 1) * self.w - self.step_alpha * grad_val
+        if self.trace:
+            self.history['time'].append(time.time() - start_time)
+            self.history['func'].append(func_val_curr)
+        iteration += 1
+        func_val_prev = func_val_curr
+        func_val_curr = self.get_objective(self.X, self.y)
+
+        while (iteration < self.max_iter) and \
+                (np.abs(func_val_curr - func_val_prev) >= self.tol):
+            func_val_prev = func_val_curr
+            grad_val = self.get_gradient(self.X, self.y)
+            self.w = (self.step_beta + 1) * self.w - self.step_alpha * grad_val
+            if self.trace:
+                self.history['time'].append(time.time() - start_time)
+                self.history['func'].append(func_val_curr)
+                start_time = time.time()
+            iteration += 1
+            func_val_curr = self.get_objective(self.X, self.y)
+
+        if self.trace:
+            self.history['time'].append(time.time() - start_time)
+            self.history['func'].append(func_val_curr)
+            return self.history
 
     def predict(self, X):
         """
@@ -54,7 +116,10 @@ class GDClassifier:
 
         return: одномерный numpy array с предсказаниями
         """
-        pass
+
+        predictions = X.dot(self.w)
+        predictions = np.where(predictions > 0, 1, -1)
+        return predictions
 
     def predict_proba(self, X):
         """
@@ -65,7 +130,9 @@ class GDClassifier:
         return: двумерной numpy array, [i, k] значение соответветствует вероятности
         принадлежности i-го объекта к классу k
         """
-        pass
+
+        proba = spec.expit(X.dot(self.w))
+        return np.array([proba, 1 - proba]).T
 
     def get_objective(self, X, y):
         """
@@ -76,7 +143,8 @@ class GDClassifier:
 
         return: float
         """
-        pass
+
+        return self.oracle.func(X, y, self.w)
 
     def get_gradient(self, X, y):
         """
@@ -87,13 +155,15 @@ class GDClassifier:
 
         return: numpy array, размерность зависит от задачи
         """
-        pass
+
+        return self.oracle.grad(X, y, self.w)
 
     def get_weights(self):
         """
         Получение значения весов функционала
         """
-        pass
+
+        return self.w
 
 
 class SGDClassifier(GDClassifier):
@@ -127,7 +197,23 @@ class SGDClassifier(GDClassifier):
 
         **kwargs - аргументы, необходимые для инициализации
         """
-        pass
+
+        if loss_function == 'binary_logistic':
+            if 'l2_coef' in kwargs.keys():
+                self.oracle = oracles.BinaryLogistic(l2_coef=kwargs['l2_coef'])
+            else:
+                self.oracle = oracles.BinaryLogistic()
+        self.step_alpha = step_alpha
+        self.step_beta = step_beta
+        self.tol = tolerance
+        self.max_iter = max_iter
+        self.seed = random_seed
+        self.batch_size = batch_size
+        self.w = None
+        self.X = None
+        self.y = None
+        self.trace = None
+        self.history = None
 
     def fit(self, X, y, w_0=None, trace=False, log_freq=1):
         """
@@ -156,4 +242,61 @@ class SGDClassifier(GDClassifier):
         history['weights_diff']: list of floats, содержит квадрат нормы разности векторов весов с соседних замеров
         (0 для самой первой точки)
         """
-        pass
+
+        np.random.seed(self.seed)
+        self.X = X
+        self.y = y
+        if w_0 is None:
+            self.w = np.random.uniform(0, 1, X.shape[1])  # need to make it with random
+        else:
+            self.w = w_0
+        self.trace = trace
+        if self.trace:
+            self.history = {'epoch_num': [], 'time': [], 'func': [], 'weights_diff': [0]}
+
+        # Algorithm
+        iteration = 0
+        relative_epoch_num = 0
+        start_time = time.time()
+        full_indices = np.random.choice(self.X.shape[0] * self.max_iter)
+
+        indices = full_indices[iteration * self.batch_size: (iteration + 1) * self.batch_size]
+        X_batch, y_batch = self.X[indices], self.y[indices]
+        func_val_curr = self.get_objective(X_batch, y_batch)
+        grad_val = self.get_gradient(X_batch, y_batch)
+        self.w = (self.step_beta + 1) * self.w - self.step_alpha * grad_val
+        if self.trace:
+            self.history['epoch_num'].append(relative_epoch_num)
+            self.history['time'].append(time.time() - start_time)
+            self.history['func'].append(func_val_curr)
+            self.history['weights_diff'].append(0)
+        iteration += 1
+        func_val_prev = func_val_curr
+        relative_epoch_num_prev = 0
+
+        indices = full_indices[iteration * self.batch_size:
+                               (iteration + 1) * self.batch_size]
+        X_batch, y_batch = self.X[indices], self.y[indices]
+        func_val_curr = self.get_objective(X_batch, y_batch)
+        while (iteration < self.max_iter) and \
+                (np.abs(func_val_curr - func_val_prev) >= self.tol):
+            start_time = time.time()
+            grad_val = self.get_gradient(X_batch, y_batch)
+            self.w = (1 + self.step_beta) * self.w - self.step_alpha * grad_val
+            if log_freq < ((relative_epoch_num -
+                            relative_epoch_num_prev) / self.X.shape[0]):
+                relative_epoch_num_prev = relative_epoch_num
+                func_val_prev = func_val_curr
+                func_val_curr = self.get_objective(X_batch, y_batch)
+                if self.trace:
+                    self.history['epoch_num'].append(relative_epoch_num)
+                    self.history['time'].append(time.time() - start_time)
+                    self.history['func'].append(func_val_curr)
+                    prev_weights = self.history['weights_diff'][-1]
+                    weights_norm = np.linalg.norm(prev_weights - self.w)
+                    self.history['weights_diff'].append(weights_norm)
+            iteration += 1
+            indices = full_indices[iteration * self.batch_size: (iteration + 1) * self.batch_size]
+            X_batch, y_batch = self.X[indices], self.y[indices]
+        if self.trace:
+            return self.history
