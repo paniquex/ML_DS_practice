a4525ff6750b8da8f77c0eef99b4a5ce75489d01
Nikita <paniquexx@gmail.com>
2019-12-19 13:09:27 +0300

    RandomForestMSE was realized

--- PRACTICUM/LAB_07/ensembles.py
+++ PRACTICUM/LAB_07/ensembles.py
@@ -9,36 +9,69 @@ class RandomForestMSE:
         """
         n_estimators : int
             The number of trees in the forest.
-        
+
         max_depth : int
             The maximum depth of the tree. If None then there is no limits.
-        
+
         feature_subsample_size : float
             The size of feature set for each tree. If None then use recommendations.
         """
-        pass
-        
+
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        if feature_subsample_size is None:
+            self.feature_subsample_size = 1
+        else:
+            self.feature_subsample_size = feature_subsample_size
+        self.trees_parameters = trees_parameters
+
     def fit(self, X, y):
         """
         X : numpy ndarray
             Array of size n_objects, n_features
-            
+
         y : numpy ndarray
             Array of size n_objects
         """
-        pass
-        
+
+        alpha = 1
+
+        indexes_obj_all = np.arange(X.shape[0])
+        indexes_feat_all = np.arange(X.shape[1])
+        self.tree_models = []
+        for i in range(self.n_estimators):
+            indexes_obj_subset = np.random.choice(indexes_obj_all,
+                                                  size=int(alpha * X.shape[0]),
+                                                  replace=False)
+            indexes_feat_subset = np.random.choice(indexes_feat_all,
+                                                   size=int(self.feature_subsample_size * X.shape[1]),
+                                                   replace=False)
+            dec_tree = None
+            dec_tree = DecisionTreeRegressor(*self.trees_parameters,
+                                             max_depth=self.max_depth,
+                                             )
+            dec_tree.fit(X[indexes_obj_subset[:, None], indexes_feat_subset],
+                         y[indexes_obj_subset])
+            self.tree_models.append((dec_tree, indexes_feat_subset))
+            del dec_tree
+
     def predict(self, X):
         """
         X : numpy ndarray
             Array of size n_objects, n_features
-            
+
         Returns
         -------
         y : numpy ndarray
             Array of size n_objects
         """
-        pass
+
+        preds = np.zeros(X.shape[0])
+        indexes_all = np.arange(X.shape[0])
+        for i in range(self.n_estimators):
+            preds += self.tree_models[i][0].predict(X[indexes_all[:, None],
+                                                      self.tree_models[i][1]]) / self.n_estimators
+        return preds
 
 
 class GradientBoostingMSE:
@@ -47,23 +80,23 @@ class GradientBoostingMSE:
         """
         n_estimators : int
             The number of trees in the forest.
-        
+
         learning_rate : float
             Use learning_rate * gamma instead of gamma
 
         max_depth : int
             The maximum depth of the tree. If None then there is no limits.
-        
+
         feature_subsample_size : float
             The size of feature set for each tree. If None then use recommendations.
         """
         pass
-        
+
     def fit(self, X, y):
         """
         X : numpy ndarray
             Array of size n_objects, n_features
-            
+
         y : numpy ndarray
             Array of size n_objects
         """
@@ -73,7 +106,7 @@ class GradientBoostingMSE:
         """
         X : numpy ndarray
             Array of size n_objects, n_features
-            
+
         Returns
         -------
         y : numpy ndarray
